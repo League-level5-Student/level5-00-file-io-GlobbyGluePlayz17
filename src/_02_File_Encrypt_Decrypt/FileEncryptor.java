07b7ca1d02621652a8fdad4bebe544a81e8764e1
GlobbyGluePlayz17 <dudisd888@gmail.com>
2020-04-18 16:02:59 -0700

    Intro, Record, ED (ip)

--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,5 +1,10 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileEncryptor {
 	/*
 	 * Encryption is the process of encoding a message or information
@@ -20,4 +25,64 @@ public class FileEncryptor {
 	 * Create a program that takes a message and a key from the user.
 	 * Use the key to shift each letter in the users input and save the final result to a file.
 	 */
+	
+	public static void main(String[] args) {
+		FileEncryptor fe1 = new FileEncryptor();
+		fe1.MessageEncryptor();
+	}
+	
+	public void MessageEncryptor() {
+		String msg = JOptionPane.showInputDialog("Please enter in the message you wish to encrypt:");
+		String tempkey = JOptionPane.showInputDialog("Enter in the key you wish to use to encrypt your message: (only integers)");
+		int key = Integer.parseInt(tempkey);
+		
+		Character[] alphabetLC = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+		Character[] alphabetUC = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+		
+		String encryptedmsg = "";
+		for (int i = 0; i < msg.length(); i++) {
+			boolean lettercheck = false;
+			boolean found = false;
+			
+			if (msg.charAt(i).toUpperCase == true) {
+				
+			}
+			for (int j = 0; j < alphabetLC.length; j++) {
+				if (msg.charAt(i) == alphabetLC[j]) {					
+					if (j+key > alphabetLC.length) {
+						encryptedmsg = encryptedmsg + alphabetLC[(j+key)-alphabetLC.length];
+						lettercheck = true;
+						found = true;
+						break;
+					} else if (j+key < 0) {
+						encryptedmsg = encryptedmsg + alphabetLC[alphabetLC.length-(j+key)];
+						lettercheck = true;
+						found = true;
+						break;
+					} else {
+						encryptedmsg = encryptedmsg + alphabetLC[j+key];
+						lettercheck = true;
+						found = true;
+						break;
+					}
+					
+				} 
+			} 
+			
+			if (!lettercheck) {
+				encryptedmsg = encryptedmsg + msg.charAt(i);
+			}
+		}
+		
+		try {
+			FileWriter fw = new FileWriter("src/_02_File_Encrypt_Decrypt/Encrypt", true);
+		
+			fw.write("\n" + encryptedmsg + "\n");
+			fw.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+				
+	}
 }
